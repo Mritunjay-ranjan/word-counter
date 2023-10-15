c9cdd0381511555bd8bde79e98b20a1e04364675
Kumar Mritunjay <kumarmritunjay@apples-MacBook-Air.local>
2023-10-15 19:36:42 +0530

    change app name

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState('');
+  const wordCount = text.split(' ').filter(word => word !== '').length;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Responsive Paragraph Word Counter</h1>
+      <textarea
+        value={text}
+        onChange={e => setText(e.target.value)}
+        placeholder="Type here..."
+      />
+      <p>Word Count: {wordCount}</p>
     </div>
   );
 }
